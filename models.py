9744d2a421613822135ef8c0eaf92be9f6564891
Elliott Seyler <emhs08@gmail.com>
2013-09-04 21:35:48 -0700

    Abandoning Mongo for SQL and Storm.

--- models.py
+++ models.py
@@ -1,6 +1,4 @@
-from minimongo import Model, Index, configure
-
-configure(host=u'paulo.mongohq.com', port=10067, username=u"emhs", password=u'dea84638ime', database='Exhumator')
+from storm.locals import *
 
 models = []
 actions = ["goto", "click", "select", "text", "frame", "dump"]
@@ -17,27 +15,3 @@ def expanded_calls(step):
     for args_combo in itertools.product(*listified_args):
         yield step[0], args_combo
 
-class Source(Model):
-    class Meta:
-        collection="sources"
-
-    def __init__(self, name):
-        super(Source, self)
-        
-        # Can't use self.attr until instantiated
-        self.__setitem__("name", name)
-        self.__setitem__("company", None)
-        self.__setitem__("steps", [])
-        self.__setitem__("funeral_homes", [])
-
-#    def add_step(self, action, target, value=None):
-#        if action not in actions:
-#            raise TypeError("Not a valid action")
-#        elif action in actions_with_values:
-#            if value is None:
-#                raise TypeError("Action \"{action}\" requires a value".format(action=action))
-#            elif if not isinstance(value, list):
-#                raise TypeError("Values must be expressed as a list")
-#        else:
-
-models.append(Source)
