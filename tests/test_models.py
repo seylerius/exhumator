4af5d8438fbe79f164dd4ade085e1932c62a6d11
Elliott Seyler <emhs08@gmail.com>
2013-09-06 21:33:33 -0700

    Write tests for Source class

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,24 @@
 from . import models
 import pytest
+from storm.locals import *
+
+@pytest.fixture()
+def store(scope='module'):
+    database = create_database('sqlite:')
+    store = Store(database)
+    store.execute("CREATE TABLE source "
+                  "(id INTEGER PRIMARY KEY, name VARCHAR, url VARCHAR)")
+    return store
 
 class TestSource:
+    def test_create(self, store):
+        test_source = Source()
+        test_source.name = "Joe's Funerals.com"
+        test_source.url = "http://www.joesfunerals.com"
+        store(test_source)
+        store.flush()
+        assert "{source!r}".format(source=test_source) == "Source(name=\"Joe's Funerals.com\", url=\"http://www.joesfunerals.com\", id=1)"
+
+    def test_find(self, store):
+        test_source = store.find(models.Source, models.Source.name == "Joe's Funerals.com").one()
+        assert "{source!r}".format(source=test_source) == "Source(name=\"Joe's Funerals.com\", url=\"http://www.joesfunerals.com\", id=1)"
